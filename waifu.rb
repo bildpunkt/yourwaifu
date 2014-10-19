94e3527f0ba4091d9f2c9dc07687ccccf2462ebc
pixeldesu <pixeldesu@nerdwerks.org>
2014-10-19 21:56:48 +0200

    still trying to fix that one mistake

--- waifu.rb
+++ waifu.rb
@@ -56,27 +56,27 @@ loop do
       unless filtered_users.nil?
         puts "\033[32;1m[#{Time.new.to_s}] #{filtered_users} is filtered\033[0m"
         else
-        unless filtered_words.nil?
-          puts "\033[32;1m[#{Time.new.to_s}] #{object.user.screen_name} triggered filter: '#{filtered_words}'\033[0m"
-          else
-            chosen_one = waifu.sample
-            puts "[#{Time.new.to_s}] #{object.user.screen_name}: #{chosen_one["name"]} - #{chosen_one["series"]}"
-            begin
-              begin
-                client.update_with_media "@#{object.user.screen_name} Your waifu is #{chosen_one["name"]} (#{chosen_one["series"]})", File.new("img/#{chosen_one["series"]}/#{chosen_one["name"]}.png"), in_reply_to_status:object
-              rescue Exception => m
-                puts "\033[34;1m[#{Time.new.to_s}] #{m.message} Trying to post tweet without image!\033[0m"
-                client.update "@#{object.user.screen_name} Your waifu is #{chosen_one["name"]} (#{chosen_one["series"]})", in_reply_to_status:object
+          unless filtered_words.nil?
+            puts "\033[32;1m[#{Time.new.to_s}] #{object.user.screen_name} triggered filter: '#{filtered_words}'\033[0m"
+            else
+              chosen_one = waifu.sample
+              puts "[#{Time.new.to_s}] #{object.user.screen_name}: #{chosen_one["name"]} - #{chosen_one["series"]}"
+                begin
+                  begin
+                    client.update_with_media "@#{object.user.screen_name} Your waifu is #{chosen_one["name"]} (#{chosen_one["series"]})", File.new("img/#{chosen_one["series"]}/#{chosen_one["name"]}.png"), in_reply_to_status:object
+                  rescue Exception => m
+                    puts "\033[34;1m[#{Time.new.to_s}] #{m.message} Trying to post tweet without image!\033[0m"
+                    client.update "@#{object.user.screen_name} Your waifu is #{chosen_one["name"]} (#{chosen_one["series"]})", in_reply_to_status:object
+                  end
+                rescue Exception => e
+                  puts "\033[31;1m[#{Time.new.to_s}] #{e.message}\033[0m"
+                end
               end
-            rescue Exception => e
-              puts "\033[31;1m[#{Time.new.to_s}] #{e.message}\033[0m"
             end
           end
         end
       end
-      end
     end
   end
-end
   sleep 1
 end
