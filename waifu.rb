0b553f6ec482d1c928a48a5649ea01b891aa1851
nilsding <nilsding@nilsding.org>
2014-10-20 20:25:38 +0200

    restructured the code a bit

--- waifu.rb
+++ waifu.rb
@@ -1,10 +1,11 @@
+#!/usr/bin/env ruby
 require "yaml"
 require "twitter"
 require "ostruct"
 
 keys = YAML.load_file File.expand_path(".", "config.yml")
-filter_words = YAML.load_file File.expand_path(".", "filter_words.yml")
-filter_users = YAML.load_file File.expand_path(".", "filter_users.yml")
+FILTER_WORDS = YAML.load_file File.expand_path(".", "filter_words.yml")
+FILTER_USERS = YAML.load_file File.expand_path(".", "filter_users.yml")
 waifu = YAML.load_file File.expand_path(".", "waifu.yml")
 
 client = Twitter::REST::Client.new do |config|
@@ -22,67 +23,86 @@ streamer = Twitter::Streaming::Client.new do |config|
 end
 
 begin
-  current_user = client.current_user
+  $current_user = client.current_user
 rescue Exception => e
   puts "Exception: #{e.message}"
   # best hack:
-  current_user = OpenStruct.new
-  current_user.id = keys["access_token"].split("-")[0].to_i
+  $current_user = OpenStruct.new
+  $current_user.id = keys["access_token"].split("-")[0].to_i
 end
 
 puts "yourwaifu"
 puts "-------------------------------"
 puts "serving \033[34;1m#{waifu.count}\033[0m entries"
-puts "filtering with \033[32;1m#{filter_words.count}\033[0m entries"
-# puts "filtering \033[36;1m#{filter_users.count}\033[0m users"
+puts "filtering with \033[32;1m#{FILTER_WORDS.count}\033[0m entries"
+puts "filtering \033[36;1m#{FILTER_USERS.count}\033[0m users"
 puts "-------------------------------"
 
-loop do
-  streamer.user do |object|
-    if object.is_a? Twitter::Tweet
-      unless current_user.id == object.user.id
-        unless object.text.start_with? "RT @"
-=begin
-          filtered_users = nil
-          filter_users.each do |fu|
-            if object.user.screen_name.downcase.include? fu.downcase
-              filtered_users = fu
-          break
-=end
-          filtered_words = nil 
-          filter_words.each do |fw|
-            if object.text.downcase.include? fw.downcase
-              filtered_words = fw
-          break
-        end
+class NotImportantException < Exception
+end
+
+class FilteredException < Exception
+end
+
+class FilteredUserException < FilteredException
+end
+
+class FilteredTweetException < FilteredException
+end
+
+class Twitter::Tweet
+  def raise_if_current_user!
+    raise NotImportantException if $current_user.id == self.user.id
+  end
+
+  def raise_if_retweet!
+    raise NotImportantException if self.text.start_with? "RT @"
+  end
+
+  def raise_if_word_filtered!
+    FILTER_WORDS.each do |fw|
+      if self.text.downcase.include? fw.downcase
+        raise FilteredUserException, "#{self.user.screen_name} triggered filter: '#{fw}'"
       end
-=begin
-      unless filtered_users.nil?
-        puts "\033[36;1m[#{Time.new.to_s}] #{filtered_users} is filtered\033[0m"
-        else
-=end
-          unless filtered_words.nil?
-            puts "\033[32;1m[#{Time.new.to_s}] #{object.user.screen_name} triggered filter: '#{filtered_words}'\033[0m"
-            else
-              chosen_one = waifu.sample
-              puts "[#{Time.new.to_s}] #{object.user.screen_name}: #{chosen_one["name"]} - #{chosen_one["series"]}"
-                begin
-                  begin
-                    client.update_with_media "@#{object.user.screen_name} Your waifu is #{chosen_one["name"]} (#{chosen_one["series"]})", File.new("img/#{chosen_one["series"]}/#{chosen_one["name"]}.png"), in_reply_to_status:object
-                  rescue Exception => m
-                    puts "\033[34;1m[#{Time.new.to_s}] #{m.message} Trying to post tweet without image!\033[0m"
-                    client.update "@#{object.user.screen_name} Your waifu is #{chosen_one["name"]} (#{chosen_one["series"]})", in_reply_to_status:object
-                  end
-                rescue Exception => e
-                  puts "\033[31;1m[#{Time.new.to_s}] #{e.message}\033[0m"
-                end
-             #  end
-           #  end
-         #  end
-        end
+    end
+  end
+  
+  def raise_if_user_filtered!
+    FILTER_USERS.each do |fu|
+      if self.user.screen_name.downcase.include? fu.downcase
+        raise FilteredTweetException, "#{self.user.screen_name} got filtered"
       end
     end
   end
 end
+
+loop do
+  streamer.user do |object|
+    if object.is_a? Twitter::Tweet
+      begin
+        object.raise_if_current_user!
+        object.raise_if_retweet!
+        object.raise_if_word_filtered!
+        object.raise_if_user_filtered!
+        
+        chosen_one = waifu.sample
+        puts "[#{Time.new.to_s}] #{object.user.screen_name}: #{chosen_one["name"]} - #{chosen_one["series"]}"
+        if File.exists? File.expand_path("../img/#{chosen_one["series"]}/#{chosen_one["name"]}.png", __FILE__)
+          client.update_with_media "@#{object.user.screen_name} Your waifu is #{chosen_one["name"]} (#{chosen_one["series"]})", File.new("img/#{chosen_one["series"]}/#{chosen_one["name"]}.png"), in_reply_to_status:object
+          puts "[#{Time.new.to_s}] posted with image!"
+        else
+          client.update "@#{object.user.screen_name} Your waifu is #{chosen_one["name"]} (#{chosen_one["series"]})", in_reply_to_status:object
+          puts "[#{Time.new.to_s}] posted without image!"
+        end
+      rescue NotImportantException => e
+      rescue FilteredTweetException => e
+        puts "\033[32;1m[#{Time.new.to_s}] #{e.message}\033[0m"
+      rescue FilteredUserException => e
+        puts "\033[36;1m[#{Time.new.to_s}] #{e.message}\033[0m"
+      rescue Exception => e
+        puts "\033[31;1m[#{Time.new.to_s}] #{e.message}\033[0m"
+      end
+    end
+  end
   sleep 1
 end
