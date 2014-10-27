56ce8304010ae4c66bd3d361f2952c539f347388
pixeldesu <pixeldesu@nerdwerks.org>
2014-10-27 02:25:18 +0100

    increment revision

--- waifu.rb
+++ waifu.rb
@@ -4,7 +4,7 @@ require "twitter"
 require "tumblr_client"
 require "ostruct"
 
-version = "v2.0.3"
+version = "v2.0.4"
 
 # loading the config file
 keys = YAML.load_file File.expand_path(".", "config.yml")
