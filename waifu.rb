5faffc1a31159fe45473d9212ea20ec43c362189
pixeldesu <git@pixelde.su>
2015-08-12 19:40:50 +0200

    move Twitter extensions into seperate file

--- waifu.rb
+++ waifu.rb
@@ -7,12 +7,20 @@ require "ostruct"
 version = "v3.0.0"
 
 # loading the config file
-keys = YAML.load_file File.expand_path(".", "config.yml")
+KEYS = YAML.load_file File.expand_path(".", "config.yml")
+
+# regex to get client name
+SOURCE_REGEX = /^<a href=\"(https?:\/\/\S+|erased_\d+)\" rel=\"nofollow\">(.+)<\/a>$/
 
 # loading the filter lists
 FILTER_WORDS = YAML.load_file File.expand_path(".", "filters/words.yml")
 FILTER_USERS = YAML.load_file File.expand_path(".", "filters/users.yml")
 FILTER_CLIENTS = YAML.load_file File.expand_path(".", "filters/clients.yml")
+
+# loading scripts
+$LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
+
+require 'twitter-extensions'
 
 # loading the lists containing characters
 waifu = YAML.load_file File.expand_path(".", "lists/waifu.yml")
@@ -23,41 +31,38 @@ touhou = YAML.load_file File.expand_path(".", "lists/touhou.yml")
 vocaloid = YAML.load_file File.expand_path(".", "lists/vocaloid.yml")
 idol = YAML.load_file File.expand_path(".", "lists/idol.yml")
 
-# regex to get client name
-SOURCE_REGEX = /^<a href=\"(https?:\/\/\S+|erased_\d+)\" rel=\"nofollow\">(.+)<\/a>$/
-
 # Twitter client configuration
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key = keys['twitter']['consumer_key']
-  config.consumer_secret = keys['twitter']['consumer_secret']
-  config.access_token = keys['twitter']['access_token']
-  config.access_token_secret = keys['twitter']['access_token_secret']
+  config.consumer_key = KEYS['twitter']['consumer_key']
+  config.consumer_secret = KEYS['twitter']['consumer_secret']
+  config.access_token = KEYS['twitter']['access_token']
+  config.access_token_secret = KEYS['twitter']['access_token_secret']
 end
 
 streamer = Twitter::Streaming::Client.new do |config|
-  config.consumer_key = keys['twitter']['consumer_key']
-  config.consumer_secret = keys['twitter']['consumer_secret']
-  config.access_token = keys['twitter']['access_token']
-  config.access_token_secret = keys['twitter']['access_token_secret']
+  config.consumer_key = KEYS['twitter']['consumer_key']
+  config.consumer_secret = KEYS['twitter']['consumer_secret']
+  config.access_token = KEYS['twitter']['access_token']
+  config.access_token_secret = KEYS['twitter']['access_token_secret']
 end
 
 # OPTIONAL: Second Twitter configuration (status account)
-if keys['statustwitter']['enabled']
+if KEYS['statustwitter']['enabled']
   status = Twitter::REST::Client.new do |config|
-    config.consumer_key = keys['statustwitter']['consumer_key']
-    config.consumer_secret = keys['statustwitter']['consumer_secret']
-    config.access_token = keys['statustwitter']['access_token']
-    config.access_token_secret = keys['statustwitter']['access_token_secret']
+    config.consumer_key = KEYS['statustwitter']['consumer_key']
+    config.consumer_secret = KEYS['statustwitter']['consumer_secret']
+    config.access_token = KEYS['statustwitter']['access_token']
+    config.access_token_secret = KEYS['statustwitter']['access_token_secret']
   end
 end
 
 # OPTIONAL: Tumblr configuration
-if keys['tumblr']['enabled']
+if KEYS['tumblr']['enabled']
   Tumblr.configure do |config|
-    config.consumer_key = keys['tumblr']['consumer_key']
-    config.consumer_secret = keys['tumblr']['consumer_secret']
-    config.oauth_token = keys['tumblr']['access_token']
-    config.oauth_token_secret = keys['tumblr']['access_token_secret']
+    config.consumer_key = KEYS['tumblr']['consumer_key']
+    config.consumer_secret = KEYS['tumblr']['consumer_secret']
+    config.oauth_token = KEYS['tumblr']['access_token']
+    config.oauth_token_secret = KEYS['tumblr']['access_token_secret']
   end
 
   tumblr_client = Tumblr::Client.new
@@ -67,14 +72,7 @@ limited = false
 otp = {status: false, type: ""}
 chosen_one = {}
 
-begin
-  $current_user = client.current_user
-rescue Exception => e
-  puts "Exception: #{e.message}"
-  # best hack:
-  $current_user = OpenStruct.new
-  $current_user.id = keys['twitter']["access_token"].split("-")[0].to_i
-end
+$current_user = client.get_current_user
 
 puts "yourwaifu #{version}"
 puts "-------------------------------"
@@ -90,64 +88,13 @@ puts "Filters: [ \033[33;1m#{FILTER_WORDS.count}\033[0m] words"
 puts "         [ \033[33;1m#{FILTER_USERS.count}\033[0m] users"
 puts "         [  \033[33;1m#{FILTER_CLIENTS.count}\033[0m] clients"
 puts "-------------------------------"
-if keys['tumblr']['enabled']
+if KEYS['tumblr']['enabled']
   puts "\033[36;1mposting to Tumblr if status limit occurs\033[0m"
   puts "-------------------------------"
 end
-if keys['statustwitter']['enabled']
+if KEYS['statustwitter']['enabled']
   puts "\033[36;1mposting to Twitter if status limit occurs\033[0m"
   puts "-------------------------------"
-end
-
-
-class NotImportantException < Exception
-end
-
-class FilteredException < Exception
-end
-
-class FilteredClientException < FilteredException
-end
-
-class FilteredUserException < FilteredException
-end
-
-class FilteredTweetException < FilteredException
-end
-
-class Twitter::Tweet
-  def raise_if_current_user!
-    raise NotImportantException if $current_user.id == self.user.id
-  end
-
-  def raise_if_retweet!
-    raise NotImportantException if self.text.start_with? "RT @"
-  end
-
-  def raise_if_client_filtered!
-    FILTER_CLIENTS.each do |fc|
-      filter_client = self.source.match SOURCE_REGEX
-      if filter_client[2].downcase.include? fc.downcase
-        raise FilteredClientException, "#{self.user.screen_name} is replying with #{fc}, a filtered client"
-      end
-    end
-  end
-
-  def raise_if_word_filtered!
-    FILTER_WORDS.each do |fw|
-      if self.text.downcase.include? fw.downcase
-        raise FilteredTweetException, "#{self.user.screen_name} triggered filter: '#{fw}'"
-      end
-    end
-  end
-
-  def raise_if_user_filtered!
-    FILTER_USERS.each do |fu|
-      if self.user.screen_name.downcase.include? fu.downcase
-        raise FilteredUserException, "#{self.user.screen_name} is filtered, not going to reply"
-      end
-    end
-  end
 end
 
 loop do
@@ -181,7 +128,7 @@ loop do
               otp['type'] = "shipgirl"
               chosen_one['partner_a'] = shipgirl.sample
               chosen_one['partner_b'] = shipgirl.sample
-              chosen_one['title'] = "OTP"  
+              chosen_one['title'] = "OTP"
             end
           when /touhou/i
             chosen_one = touhou.sample
@@ -191,7 +138,7 @@ loop do
               otp['type'] = "touhou"
               chosen_one['partner_a'] = touhou.sample
               chosen_one['partner_b'] = touhou.sample
-              chosen_one['title'] = "OTP" 
+              chosen_one['title'] = "OTP"
             end
           when /vocaloid/i
             chosen_one = vocaloid.sample
@@ -201,7 +148,7 @@ loop do
               otp['type'] = "vocaloid"
               chosen_one['partner_a'] = vocaloid.sample
               chosen_one['partner_b'] = vocaloid.sample
-              chosen_one['title'] = "OTP" 
+              chosen_one['title'] = "OTP"
             end
           when /idol/i
             chosen_one = idol.sample
@@ -211,8 +158,8 @@ loop do
               otp['type'] = "idol"
               chosen_one['partner_a'] = idol.sample
               chosen_one['partner_b'] = idol.sample
-              chosen_one['title'] = "OTP" 
-            end   
+              chosen_one['title'] = "OTP"
+            end
           when /yuri otp/i
             otp['status'] = true
             otp['type'] = "yuri"
@@ -251,7 +198,7 @@ loop do
               client.update "@#{object.user.screen_name} Your #{chosen_one["title"]} is #{chosen_one["partner_a"]["name"]} x #{chosen_one["partner_b"]["name"]} (Touhou)", in_reply_to_status: object
             when "vocaloid"
               puts "[#{Time.new.to_s}][#{otp["type"]} OTP] #{object.user.screen_name}: #{chosen_one["partner_a"]["name"]} x #{chosen_one["partner_b"]["name"]}"
-              client.update "@#{object.user.screen_name} Your #{chosen_one["title"]} is #{chosen_one["partner_a"]["name"]} x #{chosen_one["partner_b"]["name"]} (#{chosen_one["partner_a"]["series"]}|#{chosen_one["partner_b"]["series"]})", in_reply_to_status: object  
+              client.update "@#{object.user.screen_name} Your #{chosen_one["title"]} is #{chosen_one["partner_a"]["name"]} x #{chosen_one["partner_b"]["name"]} (#{chosen_one["partner_a"]["series"]}|#{chosen_one["partner_b"]["series"]})", in_reply_to_status: object
             when "yaoi"
               puts "[#{Time.new.to_s}][#{otp["type"]} OTP] #{object.user.screen_name}: #{chosen_one["partner_a"]["name"]} x #{chosen_one["partner_b"]["name"]}"
               client.update "@#{object.user.screen_name} Your #{chosen_one["title"]} is #{chosen_one["partner_a"]["name"]} x #{chosen_one["partner_b"]["name"]} (#{chosen_one["partner_a"]["series"]}|#{chosen_one["partner_b"]["series"]})", in_reply_to_status: object
@@ -274,10 +221,10 @@ loop do
         end
         if limited
           limited = false
-          if keys['tumblr']['enabled']
-            tumblr_client.text(keys['tumblr']['blog_name'], title: "I'm back!", body: "The limit is gone now and you can get waifus/husbandos again! [Bot has been unlimited since: #{Time.new.to_s}]")
+          if KEYS['tumblr']['enabled']
+            tumblr_client.text(KEYS['tumblr']['blog_name'], title: "I'm back!", body: "The limit is gone now and you can get waifus/husbandos again! [Bot has been unlimited since: #{Time.new.to_s}]")
           end
-          if keys['statustwitter']['enabled']
+          if KEYS['statustwitter']['enabled']
             status.update "You can get waifus/husbandos again! [Bot has been unlimited since: #{Time.new.to_s}]"
           end
         end
@@ -292,10 +239,10 @@ loop do
         puts "\033[31;1m[#{Time.new.to_s}] #{e.message}\033[0m"
         if e.message.match /update limit/i and !limited
           limited = true
-          if keys['tumblr']['enabled']
-            tumblr_client.text(keys['tumblr']['blog_name'], title: 'Bot is limited', body: "I've reached the \"daily\" limit for now! Please wait a bit before mentioning me again. [Bot has been limited since: #{Time.new.to_s}]")
+          if KEYS['tumblr']['enabled']
+            tumblr_client.text(KEYS['tumblr']['blog_name'], title: 'Bot is limited', body: "I've reached the \"daily\" limit for now! Please wait a bit before mentioning me again. [Bot has been limited since: #{Time.new.to_s}]")
           end
-          if keys['statustwitter']['enabled']
+          if KEYS['statustwitter']['enabled']
             status.update "\"Daily\" limit reached, please wait a bit before mentioning again! [Bot has been limited since: #{Time.new.to_s}]"
           end
         end
