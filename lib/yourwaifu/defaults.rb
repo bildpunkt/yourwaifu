e7a56f1570ec766378937b2907c7edd07413ef03
Georg G <nilsding@nilsding.org>
2015-08-12 23:13:47 +0200

    added yourwaifu/defaults.rb

---
+++ lib/yourwaifu/defaults.rb
@@ -0,0 +1,9 @@
+module YourWaifu
+  # Path to the base directory (i.e. repo root)
+  BASE_PATH = File.expand_path('../../../', __FILE__)
+  # Path to the directory containing the various lists
+  LIST_PATH = File.join(BASE_PATH, 'lists')
+  # Path containing the images.
+  IMAGE_PATH = File.join(BASE_PATH, 'img')
+end
+
