e0d48d92ffdce33a3467d2241acb828d011a19d7
pixeldesu <git@pixelde.su>
2016-04-15 20:27:40 +0200

    add views

--- app.rb
+++ app.rb
@@ -13,60 +13,64 @@ VOCALOID = YAML.load_file File.expand_path(".", "lists/vocaloid.yml")
 IDOL = YAML.load_file File.expand_path(".", "lists/idol.yml")
 
 get '/' do
-  waifu = WAIFU.sample
-  "Your waifu is #{waifu['name']} from #{waifu['series']}"
+  @waifu = WAIFU.sample
+  @tweet = URI::encode("My waifu is #{@waifu['name']}! Who is yours? Find out at")
+  haml :waifu, layout: :layout
 end
 
 get '/:list' do
   case params[:list]
     when /waifu/i
-      waifu = WAIFU.sample
-      type = "waifu"
+      @waifu = WAIFU.sample
+      @type = "waifu"
     when /husbando/i
-      waifu = HUSBANDO.sample
-      type = "husbando"
+      @waifu = HUSBANDO.sample
+      @type = "husbando"
     when /imouto/i
-      waifu = IMOUTO.sample
-      type = "imouto"
+      @waifu = IMOUTO.sample
+      @type = "imouto"
     when /shipgirl/i
-      waifu = SHIPGIRL.sample
-      type = "shipgirl"
+      @waifu = SHIPGIRL.sample
+      @type = "shipgirl"
     when /touhou/i
-      waifu = TOUHOU.sample
-      type = "touhou"
+      @waifu = TOUHOU.sample
+      @type = "touhou"
     when /vocaloid/i
-      waifu = VOCALOID.sample
-      type = "vocaloid"
+      @waifu = VOCALOID.sample
+      @type = "vocaloid"
     when /idol/i
-      waifu = IDOL.sample
-      type = "idol"
+      @waifu = IDOL.sample
+      @type = "idol"
     else
-      waifu = WAIFU.sample
-      type = "waifu"
+      @waifu = WAIFU.sample
+      @type = "waifu"
   end
-  "Your #{type} is #{waifu['name']} from #{waifu['series']}"
+  @tweet = URI::encode("My #{@type} is #{@waifu['name']}! Who is yours? Find it out at")
+  haml :list, layout: :layout
 end
 
 get '/otp/' do
-  partner_a = WAIFU.sample
-  partner_b = HUSBANDO.sample
-  "Your OTP is #{partner_a['name']} x #{partner_b['name']} (#{partner_a['series']}/#{partner_b['series']})"
+  @partner_a = WAIFU.sample
+  @partner_b = HUSBANDO.sample
+  @tweet = URI::encode("My OTP is #{@partner_a['name']} x #{@partner_b['name']}! Which is yours? Find out at")
+  haml :otp, layout: :layout
 end
 
 get '/otp/:type' do
   case params[:type]
     when /yaoi/i
-      partner_a = HUSBANDO.sample
-      partner_b = HUSBANDO.sample
-      type = "yaoi OTP"
+      @partner_a = HUSBANDO.sample
+      @partner_b = HUSBANDO.sample
+      @type = "yaoi OTP"
     when /yuri/i
-      partner_a = WAIFU.sample
-      partner_b = WAIFU.sample
-      type = "yuri OTP"
+      @partner_a = WAIFU.sample
+      @partner_b = WAIFU.sample
+      @type = "yuri OTP"
     else
-      partner_a = WAIFU.sample
-      partner_b = HUSBANDO.sample
-      type = "OTP"
+      @partner_a = WAIFU.sample
+      @partner_b = HUSBANDO.sample
+      @type = "OTP"
   end
-  "Your #{type} is #{partner_a['name']} x #{partner_b['name']} (#{partner_a['series']}/#{partner_b['series']})"
+  @tweet = URI::encode("My #{@type} is #{@partner_a['name']} x #{@partner_b['name']}! Which is yours? Find out at")
+  haml :otps, layout: :layout
 end
